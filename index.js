3f32b736821585e00e15fb095954ba8fd8a4c08e
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-09-16 14:23:15 +0530

    hls streaming using ffpeg and exec

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ import cors from "cors";
 import multer from "multer";
 import { v4 as uuidv4} from "uuid";
 import path from "path";
+import fs from "fs";
+import {exec} from "child_process";
+import { stderr, stdout } from "process";
 
 const app = express();
 
 //multer middleware
 const storage = multer.diskStorage({
-  destination: function(req, res, cb) {
-    cb(null, "/.uploads")
+  destination: function(req, file, cb) {
+    cb(null, "./uploads")
   },
-  filename: function(req, res, cb) {
-    cb(null, file.fieldname + "-" + uuidv4() + path.extname)
+  filename: function(req, file, cb) {
+    cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname))
   }
 })
+
+// multer config 
+const upload = multer({ storage: storage})
 
 app.use(
   cors({
@@ -33,11 +39,45 @@ app.use((req, res, next) => {
 })
 
 app.use(express.json());
-app.use(express.urlencoded({extended: ture}));
+app.use(express.urlencoded({extended: true}));
 app.use("/uploads", express.static("uploads"));
 
 app.get("/", function (req, res) {
   res.json({message:"Video streaming POC"});
+})
+
+app.post("/upload", upload.single('file'), function (req, res) {
+  // console.log("file uploaded")
+  const lessonId = uuidv4();
+  const videoPath = req.file.path;
+  const outputPath = `./uploads/courses/${lessonId}`;
+  const hlsPath = `./${outputPath}/index.m3u8`;
+  console.log("hlsPath",hlsPath);
+
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath, {recursive: true})
+  }
+
+  //ffmpeg
+  const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
+
+  // no queue, POC, not to be used in production
+  exec(ffmpegCommand, (err, stdout, stderr) => {
+    if (err){
+      console.log(`exec error: ${err}`);
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+
+    const vidoUrl = `http://localhost:8000/uploads/courses/${lessonId}/index.m3u8`;
+
+    res.json({
+      message: "Video converted to hls format",
+      vidoUrl: vidoUrl,
+      lessonId: lessonId
+    })
+  })
+
 })
 
 app.listen(8000, function() {
